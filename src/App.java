f279243505c691c36ca8273c423eb7466cdd9360
AbeKhan <128449118+AbeKhan@users.noreply.github.com>
2023-09-25 15:29:20 -0500

    Made three different cases, workie for the moment

--- src/App.java
+++ src/App.java
@@ -6,13 +6,49 @@ public class App
     public static void main(String[] args) throws Exception 
     {
         Scanner scan = new Scanner(System.in);
+        HashMap<String, Student> students = new HashMap<String, Student>();
+        boolean exit = false;
+        
+        while (!exit)
+        {
+            menu();
 
+            String holder = scan.nextLine();
+            int input = Integer.parseInt(holder);
+            switch(input)
+            {
+            case 1:
+                System.out.println("Please enter student ID: ");
+                String ID = scan.nextLine();
+                System.out.println("Please enter Name: ");
+                String name = scan.nextLine();
+                Student a = new Student(name, ID);
+                students.put(ID, a);
+                System.out.println(students);
+                break;
+
+            case 2:
+                System.out.println("It works");
+
+            case 3:
+                System.out.println("Please enter student ID: ");
+                ID = scan.nextLine();
+                students.remove(ID);
+                break;
+            }
+            
+
+        }
+
+    }
+
+
+    public static void menu()
+    {
         System.out.println("1 - Add student");
         System.out.println("2 - Find student");
         System.out.println("3 - Delete student");
         System.out.println("4 - Show all students");
         System.out.println("5 - Exit");
-
-        String input = scan.nextLine();
     }
 }
